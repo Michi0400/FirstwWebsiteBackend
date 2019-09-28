4d07c5ccdb1ccad824d4d9f32e321f870b5efc3a
Aaron Schweig <webdesign.schweig@gmail.com>
2019-09-28 21:06:11 +0200

    Fixing Docker setup

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 
-console.log(process.env.NODE_ENV);
-
 const path = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env';
 
 dotenv.config({ path });
 
 const port = process.env.PORT || 8080;
-
-console.log(process.env.DB_HOST);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -18,8 +14,7 @@ async function bootstrap() {
       origin: 'http://localhost:4200',
     },
   });
-  console.log(port)
+  console.log(port);
   await app.listen(port);
-
 }
 bootstrap();
